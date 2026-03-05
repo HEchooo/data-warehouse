afd8d9468d79250c3752c2df54101afaa42ad00f
Sinn <SinnHuu@outlook.com>
2026-03-05 18:14:41 +0800

    新增 ads_daily_content_performance 平台整体内容表现（日表）

---
+++ ddl/ads/ads_daily_content_performance.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `my-project-8584-jetonai.decom.ads_daily_content_performance` (
+    dt DATE,
+    platform_exposure_uv INT64,
+    avg_browse_content_count_per_user NUMERIC,
+    like_total_count INT64,
+    like_rate NUMERIC,
+    follow_total_count INT64,
+    read_follow_rate NUMERIC,
+    tryon_total_count INT64,
+    read_tryon_rate NUMERIC,
+    update_time TIMESTAMP
+)
+PARTITION BY
+    dt;
