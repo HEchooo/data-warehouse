9f8cfe51aac4f7d109ad14d4d321b66e375a5719
Sinn <sinn@echooo.co>
2026-03-10 18:30:28 +0800

    新增 ads_daily_post_performance 表 内容维度日报（日 x 帖子）

---
+++ ddl/ads/ads_daily_post_performance.sql
@@ -0,0 +1,18 @@
+CREATE TABLE `my-project-8584-jetonai.decom.ads_daily_post_performance` (
+    dt DATE,
+    post_id INT64,
+    post_code STRING,
+    post_name STRING,
+    module STRING,
+    column_id STRING,
+    column_name STRING,
+    post_exposure_uv INT64,
+    like_total_count INT64,
+    like_rate NUMERIC,
+    follow_total_count INT64,
+    follow_rate NUMERIC,
+    tryon_total_count INT64,
+    update_time TIMESTAMP
+)
+PARTITION BY
+    dt;
