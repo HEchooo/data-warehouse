359d794036e76ab2e97d761ef05c13ef77d0856e
Sinn <sinn@echooo.co>
2026-03-11 15:07:19 +0800

    新增 ads_daily_post_tryon_confirm 点击 tryon_confirm 帖子维度日表

---
+++ ddl/ads/ads_daily_post_tryon_confirm.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `my-project-8584-jetonai.decom.ads_daily_post_tryon_confirm` (
+    dt DATE,
+    post_id INT64,
+    post_code STRING,
+    post_name STRING,
+    click_use_pv INT64,
+    click_use_uv INT64,
+    avg_click_use_count_per_user NUMERIC,
+    update_time TIMESTAMP
+)
+PARTITION BY
+    dt;
