08f6fce149ca42a99077947236a0b563f5ab561d
Sinn <sinn@echooo.co>
2026-03-19 19:32:04 +0800

    修改依赖路径

--- etl_run.py
+++ etl_run.py
@@ -11,7 +11,7 @@ from pathlib import Path
 
 
 SCRIPT_DIR = Path(__file__).resolve().parent
-CONFIG_PATH = SCRIPT_DIR / "etl_config.json"
+CONFIG_PATH = SCRIPT_DIR / "env" / "etl_config.json"
 
 # 在脚本内覆盖运行模式：None / "remote" / "local"
 PATH_MODE_OVERRIDE = "remote"
